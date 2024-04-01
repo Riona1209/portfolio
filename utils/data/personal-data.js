9f319065b85017522a06c2076a89bfc3b7be62d4
Riona1209 <154750938+Riona1209@users.noreply.github.com>
2024-04-01 13:01:34 +0800

    feat: change resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/16840768/Thomas-Lee',
   leetcode: "https://leetcode.com/ThomasLee/",
   devUsername: "ThomasLee",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: "https://"
 }
